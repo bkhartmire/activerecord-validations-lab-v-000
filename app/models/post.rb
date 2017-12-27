715f4c7c91f0c1a71b469e96e1be0d0488524212
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-27 17:14:52 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
   def post_must_be_clickbait
     clickbait = [ /Won't Believe/, /Secret/, /Top [0-9]*/, /Guess/ ]
     if clickbait.none? {|word| clickbait.include?(word)}
-      error.add(:title, "title must be clickbait")
+      errors.add(:title, "title must be clickbait")
     end
   end
 end
